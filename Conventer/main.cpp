7dbfec04b7d8e0a051e2229600eb800f4cbbd794
Dubrovskirill <zet10set11@gmail.com>
2026-01-10 21:53:13 +0700

    зарегистрирован ViewModel и ImageProvider в main.cpp

--- Conventer/main.cpp
+++ Conventer/main.cpp
@@ -1,6 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 
+#include "ConverterViewModel.h"
+#include "PreviewImageProvider.h"
 
 int main(int argc, char *argv[])
 {
@@ -10,12 +13,31 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    // 1. Создаем экземпляр нашей ViewModel.
+    // Она будет жить на протяжении всего времени работы приложения.
+    ConverterViewModel converterVM;
+
+    // 2. Регистрация Image Provider.
+    // Теперь QML будет знать, что при запросе ссылки "image://previews/..."
+    // нужно обращаться к нашему классу.
+    engine.addImageProvider(QLatin1String("previews"), new PreviewImageProvider(converterVM.fileModel()));
+
+    // 3. Регистрация ViewModel в контексте QML.
+    // Это позволит вам писать в коде QML просто "converterVM.addFiles(...)"
+    engine.rootContext()->setContextProperty("converterVM", &converterVM);
+
+    // 4. Регистрация Модели.
+    // Удобно иметь прямой доступ к модели для GridView/ListView
+    engine.rootContext()->setContextProperty("fileModel", converterVM.fileModel());
+
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
         &app, [url](QObject *obj, const QUrl &objUrl) {
             if (!obj && url == objUrl)
                 QCoreApplication::exit(-1);
         }, Qt::QueuedConnection);
+
     engine.load(url);
 
     return app.exec();
